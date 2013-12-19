e21ab9b40168887933a0be44560593d680247a6f
vagrant <vagrant@rails-dev-box>
2013-12-19 16:05:30 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# EnrichmentMatch
+
+An applicatiton that matches Schools with local Enrichment Teachers for their in school and after school enrichment.
+
+Made by Joy Glover
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enrichmentmatch::Application.config.secret_token = '5068db6f5a2c9fec0370f59532ff1eb730c053842e837aa0d440de4078d7230c33ffd47c9de8d63acaa9e9ca6fdc944e25645cfa5dea32467afec236e5afc4b2'
